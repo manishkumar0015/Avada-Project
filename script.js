6d802c75de4b03a2256b0af1e8f1e8983a71f6fe
Manish Mali <manishmali448278@gmail.com>
2023-12-21 00:13:22 +0530

    accordion done and image problem solved

--- script.js
+++ script.js
@@ -26,23 +26,23 @@ window.addEventListener('load', () => {
     });
   }
 });
+ 
+const accordionBtns = document.querySelectorAll(".accordion");
 
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("is-open");
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+    let content = this.nextElementSibling;
+    console.log(content);
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
     }
-  });
-}
+  };
+});
