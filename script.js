9418de1d9287450b078f7aa55523d7806454d4e6
Manish Mali <manishmali448278@gmail.com>
2023-12-20 23:20:15 +0530

    new Section

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ window.addEventListener('load', () => {
   let portfolioContainer = document.querySelector('.filter_sub_section');
   if (portfolioContainer) {
     let portfolioIsotope = new Isotope(portfolioContainer, {
-      itemSelector: '.fltr_common', filter: '.fltr1',
+      itemSelector: '.fltr_common', 
+      filter: '.fltr1',
       transitionDuration: 0,
-      
-
-
     });
 
 
@@ -28,3 +26,23 @@ window.addEventListener('load', () => {
     });
   }
 });
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
